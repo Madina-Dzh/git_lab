99043c59df376cc5cd697ed104d6a60948e24c6b
Madina <madinadzanaeva670@gmail.com>
2025-06-11 17:48:55 +0300

    added the post method

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// Создание простого сервера на Node js
-/*var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end("I'm learning git day 1...");
-}).listen(8080);*/
-
-
-// Простое приложение Express
 var express = require('express');
-var app = express(); 
+var app = express();
 var path = require('path');
+
+var bodyParser = require('body-parser');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {  
-  res.sendFile(path.join(__dirname,"index.html"));
+app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname,"index.html"));
 })
 
 app.get('/process_get', function (req, res) {
@@ -27,6 +21,15 @@ app.get('/process_get', function (req, res) {
    console.log(response);
    res.end(JSON.stringify(response));
 })
-
-var server = app.listen(5000, function () {  console.log("Express App running at http://127.0.0.1:5000/");
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+var server = app.listen(5000, function () {
+   console.log("Express App running at http://127.0.0.1:5000/");
 })
